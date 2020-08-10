4aad733e12601832b39d4d74c69c8d9329d5b848
Abhi <abhigya.sa@gmail.com>
2020-08-10 17:35:07 -0400

    return indices of an array whose values sum to the given target

---
+++ twosum.py
@@ -0,0 +1,17 @@
+def twoSum(nums, target):
+
+    if len(nums) <= 1:
+        return False
+
+    pairs = {}
+    for i in range(len(nums)):
+        if nums[i] in pairs:
+            # do something
+            return [pairs[nums[i]], i]
+        else:
+            pairs[target - nums[i]] = i
+
+nums = [2, 7, 11, 15]
+target = 9
+
+print(twoSum(nums, target))
